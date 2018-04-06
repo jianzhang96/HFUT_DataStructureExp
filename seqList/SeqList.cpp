0b0b3f062a460da57a990230b3763779aeb84b3e
LittleCatZJ <31060586+LittleCatZJ@users.noreply.github.com>
2018-04-06 20:42:15 +0800

    Update SeqList.cpp

--- seqList/SeqList.cpp
+++ seqList/SeqList.cpp
@@ -114,8 +114,8 @@ bool SeqList::listDelete(int i)
     }
     #endif // DA
     int j;
-    if(listLen<=0) return false; //¿Õ±í
-    else if(i<1 || i>listLen) //³¬½ç
+    if(listLen<=0) return false; //
+    else if(i<1 || i>listLen) //
         return false;
     else {
         for(j=i;j<listLen;j++)
